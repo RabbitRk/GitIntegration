3ccfd7726f6afb203170c9b9d0e7cafb5eb8bba9
RabbittRk <emailtork09@gmail.com>
2019-01-03 20:01:41 +0530

    my third commit

--- app/src/main/java/mark1/gmt/rk_rabbitt/myapplication/MainActivity.java
+++ app/src/main/java/mark1/gmt/rk_rabbitt/myapplication/MainActivity.java
@@ -1,15 +1,12 @@
 package mark1.gmt.rk_rabbitt.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "gifsdfsasd", Toast.LENGTH_SHORT).show();
     }
 }
