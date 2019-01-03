259c23d4eda7abefd950ccc0c73201b45bc4794f
RabbittRk <emailtork09@gmail.com>
2019-01-03 22:18:14 +0530

    my third commit

---
+++ app/src/main/java/mark1/gmt/rk_rabbitt/myapplication/EndPoints.java
@@ -0,0 +1,12 @@
+package mark1.gmt.rk_rabbitt.myapplication;
+
+/**
+ * Created by Belal on 04/11/16.
+ */
+
+public class EndPoints {
+    public static final String URL_REGISTER_DEVICE = "http://192.168.43.252/PushNotification/registerDevice.php";
+    public static final String URL_SEND_SINGLE_PUSH = "http://192.168.43.252/PushNotification/sendSinglePush.php";
+    public static final String URL_SEND_MULTIPLE_PUSH = "http://192.168.43.252/PushNotification/sendMultiplePush.php";
+    public static final String URL_FETCH_DEVICES = "http://192.168.43.252/PushNotification/getRegisteredDevices.php";
+}
