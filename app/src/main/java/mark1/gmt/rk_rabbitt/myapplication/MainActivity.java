a5c3b2ebd3a2c394ad7812c0841de7d08938a9e4
RabbittRk <emailtork09@gmail.com>
2019-01-03 19:02:01 +0530

    my second commit

--- app/src/main/java/mark1/gmt/rk_rabbitt/myapplication/MainActivity.java
+++ app/src/main/java/mark1/gmt/rk_rabbitt/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package mark1.gmt.rk_rabbitt.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "git", Toast.LENGTH_SHORT).show();
     }
 }
